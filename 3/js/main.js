5051d8367a36567ad1beee2990a63b8274135cfa
Keks <keks@htmlacademy.ru>
2022-06-22 18:25:13 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/main.js
@@ -0,0 +1,103 @@
+
+const MAX_PHOTO_COUNT = 25;
+
+const DESCRIPTION = [
+    'Дома',
+    'На улице',
+    'Чем меня кормят',
+    'С друзьями',
+    'На чиле',
+];
+
+const COMMENTS = [
+    'Всё отлично!',
+    'В целом всё неплохо. Но не всё.',
+    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const NAMES = [
+    'Кекс',
+    'Шуша',
+    'Кеша',
+    'Чарли',
+    'Васька',
+]
+
+function getRandomPositiveInteger (a, b) {
+    const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+    const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+    const result = Math.random() * (upper - lower + 1) + lower;
+    return Math.floor(result);
+  }
+
+const makePhotos = (count) => {
+    const photos = [];
+    for (let i = 1; i <= count; i++) {
+        photos.push(makePhoto(i));
+    }
+    return photos;
+};
+
+function makePhoto(arg) {
+    return ({
+        id: arg,
+        url: 'photos' + arg + '.jpg',
+        description: DESCRIPTION[getRandomPositiveInteger(0, DESCRIPTION.length -1)],
+        likes: getRandomPositiveInteger(15, 200),
+        comments: createComment(arg),
+    });
+}
+
+const makeComments = function () {
+    const COMMENTS_PHOTO = [];
+    const COMMENTS_COUNT = getRandomPositiveInteger(1, 2);
+    for (let i = 0; i < COMMENTS_COUNT; i++) {
+        COMMENTS_PHOTO.push(COMMENTS[getRandomPositiveInteger(0, COMMENTS.length - 1)])
+    }
+    return COMMENTS_PHOTO;
+}
+
+const createComment = function (num) {
+    return {
+        id: getRandomPositiveInteger((num*num),(num*num*2)),
+        avatar: 'img/avatar-' + getRandomPositiveInteger(1, 6) + '.svg',
+        message: (makeComments()).join(' '),
+        name: NAMES[getRandomPositiveInteger(0, NAMES.length - 1)],
+    }
+}
+
+const photos = makePhotos(MAX_PHOTO_COUNT);
+
+
+//Функция, возвращающая случайное целое число из переданного диапазона включительно
+function randomNumber (firstNumber, secondNumber){
+  if (firstNumber < 0 || secondNumber < 0) {
+    return 'Числа должны быть положительным!';
+  } else if ( !(Number.isInteger(firstNumber)) || !(Number.isInteger(secondNumber))){
+    return 'Числа должны быть целыми!';
+  } else if (firstNumber === secondNumber) {
+    return 'Числа не должны быть равны!';
+  } else {
+    if (firstNumber<secondNumber){
+      const random = Math.floor(Math.random()*(secondNumber-firstNumber + 1) + firstNumber);
+      return random;
+    } else {
+      //если первое число меньше второго, меняем их местами
+      const random = Math.floor(Math.random()*(firstNumber - secondNumber + 1)+ secondNumber);
+      return random;
+    }
+  }
+}
+//Тест функции
+randomNumber(1,100);
+
+//Функция для проверки максимальной длины строки
+function commentLengthCheck (stringComment, maxLength){
+  return stringComment.length <= maxLength;
+}
+//Тест функции
+commentLengthCheck('abc',3);
+
